4d2b810c70faac303a14978b7817059a4ac222e7
bochmcesar <cesarbochm@gmail.com>
2025-06-09 22:39:33 -0600

    Agrega mi archivo de tarea-sonar

---
+++ multiplicacion.h
@@ -0,0 +1,10 @@
+#ifndef MULTIPLICACION_H
+#define MULTIPLICACION_H
+
+// Definición inline para que no necesites un .cpp extra
+inline int multiplicar(int a, int b) {
+    return a * b;
+}
+
+#endif // MULTIPLICACION_H
+
